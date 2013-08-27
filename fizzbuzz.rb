14d63c324dff0c0006c8ec43e274d51d917df73f
paulogaspari <paulogaspar@me.com>
2013-08-27 14:10:39 +0100

    second update - testing phase

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,31 +1,75 @@
 
+# Exercise
 #"Write a program that prints the numbers from 1 to 100. But for multiples of three print 
 # “Fizz” instead of the number and for the multiples of five print “Buzz”. For 
 # numbers which are multiples of both three and five print “FizzBuzz”."
 
+require 'rspec'
+
+
+class Burger
+  attr_reader :options
+ 
+  def initialize(options={})
+    @options = options
+  end
+ 
+  def apply_ketchup
+    @ketchup = @options[:ketchup]
+  end
+ 
+  def has_ketchup_on_it?
+    @ketchup
+  end
+end
+ 
+ 
+describe Burger do
+  describe "#apply_ketchup" do
+    subject { burger }
+    before  { burger.apply_ketchup }
+ 
+    context "with ketchup" do
+      let(:burger) { Burger.new(:ketchup => true) }
+ 
+      it { should have_ketchup_on_it }
+    end
+ 
+    context "without ketchup" do
+      let(:burger) { Burger.new(:ketchup => false) }
+ 
+      it { should_not have_ketchup_on_it }
+    end
+  end
+end
+
+
+
+
 
 # VERSION 3
 
-clasrs Rspec_test
+# class Rspec_test
 
-def divisible_by_3?(n)
-	n % 3 == 0
-end
+# 	def divisible_by_3?(n)
+# 		n % 3 == 0
+# 	end
 
-def divisible_by_5?(n)
-	n % 5 == 0
-end
+# 	def divisible_by_5?(n)
+# 		n % 5 == 0
+# 	end
 
-(0..100).each do |n|
-	if divisible_by_3?(n) && divisible_by_5?(n)
-		puts "FizzBuzz"
-	elsif divisible_by_3?(n)
-		puts "Fizz"
-	elsif divisible_by_5?(n)
-		puts "Buzz"
-	else puts n
-	end
-end
+
+# (0..100).each do |n|
+# 	if divisible_by_3?(n) && divisible_by_5?(n)
+# 		puts "FizzBuzz"
+# 	elsif divisible_by_3?(n)
+# 		puts "Fizz"
+# 	elsif divisible_by_5?(n)
+# 		puts "Buzz"
+# 	else puts n
+# 	end
+# end
 
 
 # VERSION 1
@@ -64,16 +108,6 @@ end
 # 	n += 1
 
 # end
-
-
-
-
-#	if n % 3 == 0
-#		print "Fizz"
-#	end
-#	if n % 5 == 0
-#		print "Buzz"
-#	end
 
 
 
