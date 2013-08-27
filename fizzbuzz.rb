cd49bcff54fd4c1fbe5c416783edf5eb7a54c0c0
paulogaspari <paulogaspar@me.com>
2013-08-27 14:55:51 +0100

    one more

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,28 +6,6 @@
 
 require 'rspec'
 
-
-# class Burger
-#   attr_reader :options
- 
-#   def initialize(options={})
-#     @options = options
-#   end
- 
-#   def apply_ketchup
-#     @ketchup = @options[:ketchup]
-#   end
- 
-#   def has_ketchup_on_it?
-#     @ketchup
-#   end
-# end
- 
- 
-
-
-
-
 # VERSION 3
 
 class Rspec_test
